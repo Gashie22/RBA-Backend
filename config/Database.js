029304dfe5994d3c79c39e567d707af4e59809cd
Gashie <masogashie1@gmail.com>
2024-03-08 16:48:49 +0200

    new tasks

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 
 
-const db = new Sequelize('auth_db', 'root', 'Passw0rd', {
-    host: "localhost",
+const db = new Sequelize('freedb_Elmala', 'freedb_administrator', 'x4fHeQ7Z2ADpWu!', {
+    host: "sql.freedb.tech",
     dialect: "mysql"
 });  
   
