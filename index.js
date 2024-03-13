9366f4f25a4fadc4a1b2fa654bdd65d112b6c2d2
Gashie <masogashie1@gmail.com>
2024-03-13 16:53:15 +0200

    fresh tasks

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ app.use(session({
     saveUninitialized: true,
    store: store,
     cookie: {
-        secure: 'auto'
+        
+            httpOnly: true,
+            sameSite: 'none',  //<===
+            expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+            maxAge: 1000 * 60 * 60 * 24 * 7
+        
     }
 }));
 
