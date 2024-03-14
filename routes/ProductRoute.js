f89dc317765a561b24773894aea6983bdf1e2913
Gashie <masogashie1@gmail.com>
2024-03-14 13:03:19 +0200

    render

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -11,7 +11,7 @@ import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/products',verifyUser, getProducts);
+router.get('/products', getProducts);
 router.get('/products/:id',verifyUser, getProductById);
 router.post('/products',verifyUser, createProduct);
 router.patch('/products/:id',verifyUser, updateProduct);
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -10,7 +10,7 @@ import { verifyUser,adminOnly } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/users',verifyUser,  getUsers);
+router.get('/users',  getUsers);
 router.get('/users/:id',verifyUser,  getUserById);
 router.post('/users', verifyUser, createUser);
 router.patch('/users/:id',verifyUser,adminOnly, updateUser);
