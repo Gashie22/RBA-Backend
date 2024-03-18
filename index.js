c8688a24c20d7d742a36ec4a2e090ad3abcba431
Gashie <masogashie1@gmail.com>
2024-03-18 10:08:32 +0200

    Update cors

--- index.js
+++ index.js
@@ -36,10 +36,12 @@ app.use(session({
     }
 }));
 
-app.use(cors({
-    credentials: true,
-    origin: 'http://localhost:3000' 
-}));
+app.use(cors());
+
+// app.use(cors({
+//     credentials: true,
+//     origin: 'http://localhost:3000' 
+// }));
 app.use(express.json()); 
 app.use(UserRoute);
 app.use(ProductRoute);
--- middleware/customCorsMiddleware.js
+++ middleware/customCorsMiddleware.js
@@ -1,23 +1,23 @@
-// customCorsMiddleware.js
+// // customCorsMiddleware.js
 
-import cors from "cors";
+// import cors from "cors";
 
-const customCorsMiddleware = (req, res, next) => {
-  const allowedOrigins = ['http://localhost:3000']; // Adjust as needed
-  const options = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Enable cookies
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
+// const customCorsMiddleware = (req, res, next) => {
+//   const allowedOrigins = ['http://localhost:3000']; // Adjust as needed
+//   const options = {
+//     origin: (origin, callback) => {
+//       if (allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true, // Enable cookies
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   };
 
-  cors(options)(req, res, next);
-};
+//   cors(options)(req, res, next);
+// };
 
-export default customCorsMiddleware;
+// export default customCorsMiddleware;
