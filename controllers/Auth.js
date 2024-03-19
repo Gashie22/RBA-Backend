59dfad957168b08a2317f1f48187e8643323b30b
Gashie <masogashie1@gmail.com>
2024-03-19 10:55:05 +0200

    revised

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,15 +1,6 @@
 import User from "../models/UserModel.js";
-import cors from "cors";
 import argon2 from "argon2";
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://rba-frontend.vercel.app/");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
-app.use(cors({ origin: null , credentials :  false,allowedHeaders:['Content-Type', 'Authorization']}));
 
 export const Login = async (req, res) =>{
     const user = await User.findOne({
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,16 +1,6 @@
 import express from "express";
-import cors from 'cors';
 import {Login, logOut, Me} from "../controllers/Auth.js";
 
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://elmala.com");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
-
-app.use(cors({ origin: null , credentials :  false,allowedHeaders:['Content-Type', 'Authorization']}));
 const router = express.Router();
 
 router.get('/me', Me);
