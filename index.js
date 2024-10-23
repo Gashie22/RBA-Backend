9383fa536ef69a7c21b49b1eb4288d7fd1d483d7
Gashie <123905549+Gashie22@users.noreply.github.com>
2024-10-23 15:54:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.use(session({
 
   app.use(cors({ 
     credentials: true,
-      methods: ['GET', 'POST', 'PUT', "DELETE"],
+      methods: ['GET', 'POST', 'PUT','PATCH', "DELETE"],
     origin: "http://143.244.178.37:3000"}));
 
 app.use((req, res, next) => {
